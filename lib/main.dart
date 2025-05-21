a8c4f3f7d5356ce5ce6d3beca7eb251c443c9a2e
anas al-dharei <anas202255@gmail.com>
2025-05-22 00:03:18 +0300

    Permission Handling for Device Location

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/widget/weather_data_tile.dart';
 import 'package:weather_app/services/weather_services.dart';
 import 'package:intl/intl.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const WeatherApp());
@@ -40,24 +41,94 @@ class _WeatherPageState extends State<WeatherPage> {
   String _sunset = '';
   String _visibility = '';
   
+  @override
+  void initState() {
+    super.initState();
+    _getLocationWeather();
+  }
+
+  Future<void> _getLocationWeather() async {
+    final weatherService = WeatherService();
+    final weatherData = await weatherService.fetchWeather();
+    
+    if (weatherData['success']) {
+      final data = weatherData['data'];
+      setState(() {
+        _cityName = data['name'];
+        _temperature = data['main']['temp'].toStringAsFixed(1);
+        _tempMax = data['main']['temp_max'].toStringAsFixed(1);
+        _tempMin = data['main']['temp_min'].toStringAsFixed(1);
+        _main = data['weather'][0]['main'];
+        _pressure = data['main']['pressure'].toString();
+        _humidity = data['main']['humidity'].toString();
+        _windSpeed = data['wind']['speed'].toString();
+        _sunrise = DateFormat('hh:mm a').format(DateTime.fromMillisecondsSinceEpoch(data['sys']['sunrise'] * 1000));
+        _sunset = DateFormat('hh:mm a').format(DateTime.fromMillisecondsSinceEpoch(data['sys']['sunset'] * 1000));
+        _visibility = data['visibility'].toString();
+        
+        if(_main == 'Clear') {
+          _bgImg = 'assets/images/clear.jpg';
+          _iconImg = 'assets/icons/Clear.png';
+        } else if(_main == 'Clouds') {
+          _bgImg = 'assets/images/cloudy.jpg';
+          _iconImg = 'assets/icons/Clouds.png';
+        } else if(_main == 'Rain') {
+          _bgImg = 'assets/images/rainy.jpg';
+          _iconImg = 'assets/icons/Rain.png';
+        } else if(_main == 'Fog') {
+          _bgImg = 'assets/images/fog.jpg';
+          _iconImg = 'assets/icons/Haze.png';
+        } else if(_main == 'Thunderstorm') {
+          _bgImg = 'assets/images/thunderstorm.jpg';
+          _iconImg = 'assets/icons/Thunderstorm.png';
+        } else{
+          _bgImg = 'assets/images/haze.jpg';
+          _iconImg = 'assets/icons/Haze.png';
+        }
+      });
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(weatherData['error']),
+            backgroundColor: const Color.fromARGB(61, 124, 150, 184),
+            duration: Duration(seconds: 3),
+            action: SnackBarAction(
+              label: 'Dismiss',
+              textColor: Colors.white,
+              onPressed: () {
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              },
+            ),
+          ),
+        );
+      }
+    }
+  }
+
   getData(String cityName) async {
     final weatherService = WeatherService();
-    final response = await weatherService.getWeather(cityName);
+    var weatherData;
+    if (cityName == '') {
+      weatherData = await weatherService.fetchWeather();
+    } else {
+      weatherData = await weatherService.getWeather(cityName);
+    }
     
-    if (response['success']) {
-      final weatherData = response['data'];
+    if (weatherData['success']) {
+      final data = weatherData['data'];
       setState(() {
-        _cityName = weatherData['name'];
-        _temperature = weatherData['main']['temp'].toStringAsFixed(1);
-        _tempMax = weatherData['main']['temp_max'].toStringAsFixed(1);
-        _tempMin = weatherData['main']['temp_min'].toStringAsFixed(1);
-        _main = weatherData['weather'][0]['main'];
-        _pressure = weatherData['main']['pressure'].toString();
-        _humidity = weatherData['main']['humidity'].toString();
-        _windSpeed = weatherData['wind']['speed'].toString();
-        _sunrise = DateFormat('hh:mm a').format(DateTime.fromMillisecondsSinceEpoch(weatherData['sys']['sunrise'] * 1000));
-        _sunset = DateFormat('hh:mm a').format(DateTime.fromMillisecondsSinceEpoch(weatherData['sys']['sunset'] * 1000));
-        _visibility = weatherData['visibility'].toString();
+        _cityName = data['name'];
+        _temperature = data['main']['temp'].toStringAsFixed(1);
+        _tempMax = data['main']['temp_max'].toStringAsFixed(1);
+        _tempMin = data['main']['temp_min'].toStringAsFixed(1);
+        _main = data['weather'][0]['main'];
+        _pressure = data['main']['pressure'].toString();
+        _humidity = data['main']['humidity'].toString();
+        _windSpeed = data['wind']['speed'].toString();
+        _sunrise = DateFormat('hh:mm a').format(DateTime.fromMillisecondsSinceEpoch(data['sys']['sunrise'] * 1000));
+        _sunset = DateFormat('hh:mm a').format(DateTime.fromMillisecondsSinceEpoch(data['sys']['sunset'] * 1000));
+        _visibility = data['visibility'].toString();
         
         if(_main == 'Clear') {
           _bgImg = 'assets/images/clear.jpg';
@@ -84,7 +155,7 @@ class _WeatherPageState extends State<WeatherPage> {
       if (mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text(response['error']),
+            content: Text(weatherData['error']),
             backgroundColor: const Color.fromARGB(61, 124, 150, 184),
             duration: Duration(seconds: 3),
             action: SnackBarAction(
@@ -98,6 +169,20 @@ class _WeatherPageState extends State<WeatherPage> {
         );
       }
     }
+  }
+
+  Future<bool> _checkLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return false;
+      }
+      getData('');
+    }
+    getData('');
+    return true;
+    
   }
   @override
   Widget build(BuildContext context) {
--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
@@ -40,15 +41,43 @@ class WeatherService {
   }
 
 
-  Future<Map<String, dynamic>> fetchWeather(String cityName) async 
+  Future<Map<String, dynamic>> fetchWeather() async 
   {
     try {
+      // Check location permission
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return {
+            'success': false,
+            'error': 'Location permission denied. Please enable location services.'
+          };
+        }
+      }
+      
+      if (permission == LocationPermission.deniedForever) {
+        return {
+          'success': false,
+          'error': 'Location permission permanently denied. Please enable location in settings.'
+        };
+      }
+
+      // Check if location services are enabled
+      bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        return {
+          'success': false,
+          'error': 'Location services are disabled. Please enable location services.'
+        };
+      }
+
       print('Fetching weather for current location');
       Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
       double lat = position.latitude, lon = position.longitude;
       print('Location: lat=$lat, lon=$lon');
       
-      final url = Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey');
+      final url = Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric');
       print('Request URL: $url');
       
       final response = await http.get(url);
@@ -70,9 +99,15 @@ class WeatherService {
     } catch (e, stackTrace) {
       print('Error details: $e');
       print('Stack trace: $stackTrace');
+      if (e is TimeoutException) {
+        return {
+          'success': false,
+          'error': 'Connection timed out. Please check your internet connection.'
+        };
+      }
       return {
         'success': false,
-        'error': 'Unable to connect to weather service. Please check your internet connection.'
+        'error': 'Unable to connect to weather service. Please check your internet connection and location services.'
       };
     }
   }
